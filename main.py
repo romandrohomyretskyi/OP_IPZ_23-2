9fe81a7686d5a2fc82637d98d8d5a082e2bfd4de
romandrohomyretskyi <145002660+romandrohomyretskyi@users.noreply.github.com>
2023-11-20 22:06:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ file = open('input.txt', "r")
 l = open("output.txt", "w")
 a = file.read().split(" ")
 b = list(map(float,a))
-
+def abs(n):
+    if n >= 0:
+        return n
+    else:
+        return -n
 
 def calc(n, p):
     h= (1/n)-math.exp(n)*math.sin(p)
-    y= h/math.abs(h+1)
+    y= h/abs(h+1)
     return [n, p, h, y]
 f = calc(b[0],b[1])
 l.write(""+str(f[0])+" "+str(f[1])+" "+str(f[2])+" "+str(f[3]))
